9714d1435872eb448c5aaa0f404198cc9af526ec
ANGELVELAZ51 <106613752+ANGELVELAZ51@users.noreply.github.com>
2023-03-09 19:28:54 -0600

    Add files via upload

---
+++ SensorBigSound/Sensor big sound.ino
@@ -0,0 +1,20 @@
+
+
+const int ledPin = 2; // Pin donde se conecta
+const int sensorpin = 4; // Pin donde se cone
+
+void setup(){
+  pinMode(sensorPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+void loop(){
+  int sensorvalue = digitalRead(sensorPin); // Le
+  if (sensorvalue == HIGH) {
+    digitalwrite(ledPin, HIGH); // Encender el LED
+    delay(100); // Esperar un corto periodo de tien
+    digitalWrite(ledPin, LOW); // Apagar el LED
+    
+   // si se detecta vibr
+  }
+  delay(10); // Esperar un corto periodo de tiempo ar
+}
